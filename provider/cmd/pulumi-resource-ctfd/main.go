edfc895e928f93cdea61bc50b5e6bd1a6ce6452a
Lucas TESSON <lucastesson@protonmail.com>
2024-11-25 15:48:31 +0100

    fix: deprecated functions

--- provider/cmd/pulumi-resource-ctfd/main.go
+++ provider/cmd/pulumi-resource-ctfd/main.go
@@ -21,7 +21,7 @@ import (
 	_ "embed"
 
 	ctfd "github.com/ctfer-io/pulumi-ctfd/provider"
-	tfbridge "github.com/pulumi/pulumi-terraform-bridge/pf/tfbridge"
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/pf/tfbridge"
 )
 
 //go:embed schema-embed.json
